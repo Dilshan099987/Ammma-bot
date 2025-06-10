6b51d64d6c826f81250f761405e1ac5efdf47afe
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-10 15:53:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,10 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-const settingsPath = './settings.json';
-
-let settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
-
-function saveSettings() {
-  fs.writeFileSync(settingsPath, JSON.stringify(settings, null, 2));
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
 }
-
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "qFYm2RTQ#lzptWLsR0iMU3ubW8Z3MPXJWh2oU4qRGE8X6OYMnJRQ",
-  MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",",
-  settings,
-  saveSettings
+SESSION_ID: process.env.SESSION_ID || "qFYm2RTQ#lzptWLsR0iMU3ubW8Z3MPXJWh2oU4qRGE8X6OYMnJRQ",
+MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 };
