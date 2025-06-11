c12c8c2d68a41cb9e76ed9f4e3668ffa8665feb9
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-11 10:12:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,1 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
-SESSION_ID: process.env.SESSION_ID || "qFYm2RTQ#lzptWLsR0iMU3ubW8Z3MPXJWh2oU4qRGE8X6OYMnJRQ",
-MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
-};
